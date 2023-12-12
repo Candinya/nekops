dfda43f601c8deddd9b43609a11fad93e6188528
Nya Candy <dev@candinya.com>
2023-12-12 13:09:59 +0800

    chore: tweak basic info form

--- src/components/EditServerModal/forms/BasicInfo.tsx
+++ src/components/EditServerModal/forms/BasicInfo.tsx
@@ -29,10 +29,29 @@ const BasicInfoForm = ({ form }: InputFormProps) => (
     </Grid>
     <Grid mt="md">
       <Grid.Col span={4}>
-        <ColorInput label="Color" {...form.getInputProps("color")} />
+        <ColorInput
+          label="Color"
+          swatches={[
+            "#2e2e2e",
+            "#868e96",
+            "#fa5252",
+            "#e64980",
+            "#be4bdb",
+            "#7950f2",
+            "#4c6ef5",
+            "#228be6",
+            "#15aabf",
+            "#12b886",
+            "#40c057",
+            "#82c91e",
+            "#fab005",
+            "#fd7e14",
+          ]}
+          {...form.getInputProps("color")}
+        />
       </Grid.Col>
       <Grid.Col span={8}>
-        <TagsInput label="Tags" {...form.getInputProps("tags")} />
+        <TagsInput label="Tags" clearable {...form.getInputProps("tags")} />
       </Grid.Col>
     </Grid>
     <Textarea
