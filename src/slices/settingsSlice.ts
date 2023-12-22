6ec7d8bfb08b0df5c51072c4a3796a728c03a05f
Nya Candy <dev@candinya.com>
2023-12-22 09:57:06 +0800

    chore: move default data dir to documents

--- src/slices/settingsSlice.ts
+++ src/slices/settingsSlice.ts
@@ -3,6 +3,7 @@ import { defaultSettings, type Settings } from "@/types/settings.ts";
 import { exists, readTextFile, writeTextFile } from "@tauri-apps/plugin-fs";
 import { checkParentDir } from "@/slices/common.ts";
 import { path } from "@tauri-apps/api";
+import { documentDir } from "@tauri-apps/api/path";
 
 const SettingsFileName = "settings.json";
 
@@ -10,19 +11,21 @@ export const readSettings = createAsyncThunk(
   "settings/read",
   async (): Promise<Settings> => {
     // read from local file
-    const settingsFilePath = await path.join(
-      defaultSettings.data_dir,
-      SettingsFileName,
-    );
-    await checkParentDir(defaultSettings.data_dir);
+    const parentDir = await path.join(await documentDir(), "nekops");
+    const settingsFilePath = await path.join(parentDir, SettingsFileName);
+    await checkParentDir(parentDir);
     if (await exists(settingsFilePath)) {
       // Read and parse
       const settingsFile = await readTextFile(settingsFilePath);
       return JSON.parse(settingsFile);
     } else {
       // Initialize file
-      await writeTextFile(settingsFilePath, JSON.stringify(defaultSettings));
-      return defaultSettings;
+      const initSettings: Settings = {
+        ...defaultSettings,
+        data_dir: await path.join(parentDir, "data"),
+      };
+      await writeTextFile(settingsFilePath, JSON.stringify(initSettings));
+      return initSettings;
     }
   },
 );
@@ -31,11 +34,9 @@ export const saveSettings = createAsyncThunk(
   "settings/save",
   async (state: Settings) => {
     // save to local file
-    const settingsFilePath = await path.join(
-      defaultSettings.data_dir,
-      SettingsFileName,
-    );
-    await checkParentDir(defaultSettings.data_dir);
+    const parentDir = await path.join(await documentDir(), "nekops");
+    const settingsFilePath = await path.join(parentDir, SettingsFileName);
+    await checkParentDir(parentDir);
     await writeTextFile(settingsFilePath, JSON.stringify(state));
     await checkParentDir(state.data_dir);
     return state;
