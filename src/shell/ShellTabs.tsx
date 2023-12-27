67d98bb67974237e97951db0720617a71362f116
Nya Candy <dev@candinya.com>
2023-12-27 12:35:23 +0800

    feat: no more refs

--- src/shell/ShellTabs.tsx
+++ src/shell/ShellTabs.tsx
@@ -142,23 +142,11 @@ const ShellTabs = () => {
   const [tabsData, tabsDataHandlers] = useListState<EventNewSSHPayload>([]);
   const [tabsState, tabsStateHandlers] = useListState<ShellState>([]);
   const [tabsNewMessage, tabsNewMessageHandlers] = useListState<boolean>([]);
-  // For events binding
-  const tabsDataRef = useRef<EventNewSSHPayload[]>([]);
-  const tabsStateRef = useRef<ShellState[]>([]);
-  const tabsNewMessageRef = useRef<boolean[]>([]);
-  // Bind state : setTabsData -> tabsData -> tabsDataRef
-  useEffect(() => {
-    tabsDataRef.current = tabsData;
-  }, [tabsData]);
-  useEffect(() => {
-    tabsStateRef.current = tabsState;
-  }, [tabsState]);
-  useEffect(() => {
-    tabsNewMessageRef.current = tabsNewMessage;
-  }, [tabsNewMessage]);
 
   const [currentActiveTab, setCurrentActiveTab] = useState<string | null>(null);
+  // For state binding
   const currentActiveTabRef = useRef<string | null>(null);
+  // Bind state : setCurrentActiveTab -> currentActiveTab -> currentActiveTabRef
   useEffect(() => {
     currentActiveTabRef.current = currentActiveTab;
   }, [currentActiveTab]);
@@ -189,11 +177,9 @@ const ShellTabs = () => {
   };
 
   const closeTab = (nonce: string) => {
-    const index = tabsDataRef.current.findIndex(
-      (state) => state.nonce === nonce,
-    );
+    const index = tabsData.findIndex((state) => state.nonce === nonce);
     if (index != -1) {
-      if (tabsStateRef.current[index] === "active") {
+      if (tabsState[index] === "active") {
         setTerminateConfirmIndex(index);
         openTerminateConfirmModal();
       } else {
@@ -203,28 +189,22 @@ const ShellTabs = () => {
   };
 
   const setTabShellState = (newState: ShellState, nonce: string) => {
-    const index = tabsDataRef.current.findIndex(
-      (state) => state.nonce === nonce,
-    );
-    if (tabsStateRef.current[index] !== "terminated") {
+    const index = tabsData.findIndex((state) => state.nonce === nonce);
+    if (tabsState[index] !== "terminated") {
       tabsStateHandlers.setItem(index, newState);
     }
   };
 
   const setTabNewMessageState = (nonce: string) => {
-    const index = tabsDataRef.current.findIndex(
-      (state) => state.nonce === nonce,
-    );
-    if (currentActiveTabRef.current !== tabsDataRef.current[index].nonce) {
+    const index = tabsData.findIndex((state) => state.nonce === nonce);
+    if (currentActiveTabRef.current !== tabsData[index].nonce) {
       tabsNewMessageHandlers.setItem(index, true);
     }
   };
 
   const clearTabNewMessageState = (nonce: string) => {
-    const index = tabsDataRef.current.findIndex(
-      (state) => state.nonce === nonce,
-    );
-    if (index > -1 && tabsNewMessageRef.current[index]) {
+    const index = tabsData.findIndex((state) => state.nonce === nonce);
+    if (index > -1 && tabsNewMessage[index]) {
       tabsNewMessageHandlers.setItem(index, false);
     }
   };
