f2accf3fe27515dac0dc804168b5ccfe77630cfd
Nya Candy <dev@candinya.com>
2024-01-03 10:25:29 +0800

    feat: copy command before shell page binding is ready

--- src/pages/SSH.tsx
+++ src/pages/SSH.tsx
@@ -18,12 +18,41 @@ import {
 } from "@/events/name.ts";
 import { randomString } from "@/utils/randomString.ts";
 import ServerCardsVirtualScroll from "@/components/ServerCardsVirtualScroll.tsx";
+import { writeText } from "@tauri-apps/plugin-clipboard-manager";
 
 const SSHPage = () => {
   const servers = useSelector((state: RootState) => state.servers);
   const encryption = useSelector((state: RootState) => state.encryption);
 
+  const copySSHCommand = async (server: Server) => {
+    const command = [
+      "ssh",
+      `${server.access.regular.user || "root"}@${
+        server.access.regular.address
+      }`,
+    ];
+    if (server.access.regular.port !== 22) {
+      command.push("-p", `${server.access.regular.port}`);
+    }
+    try {
+      await writeText(command.join(" "));
+      notifications.show({
+        color: "green",
+        title: "SSH command copied!",
+        message: "Paste into your favorite shell and let's start!",
+      });
+    } catch (e) {
+      notifications.show({
+        color: "red",
+        title: "Failed to copy...",
+        message: "Maybe let the server's id to remind of something?",
+      });
+    }
+  };
+
   const startSSH = async (server: Server) => {
+    return copySSHCommand(server); // Disable shell before binding is ready
+
     // Create or open Shell window
     await openShellWindow(encryption.isUnlocked); // Disable content protection when unlocked
 
