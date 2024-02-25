8d5193ccf97d68590f9cb5e5b9392cf2318b2f82
Nya Candy <dev@candinya.com>
2024-02-25 12:56:53 +0800

    chore: tweak redux state store export & import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Layout from "./layout.tsx";
 
-import store from "@/store.ts";
+import { store } from "@/store.ts";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,7 @@ import serversReducer from "@/slices/serversSlice.ts";
 import snippetsReducer from "@/slices/snippetsSlice.ts";
 import encryptionReducer from "@/slices/encryptionSlice.ts";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     settings: settingsReducer,
     servers: serversReducer,
@@ -12,8 +12,6 @@ const store = configureStore({
     encryption: encryptionReducer,
   },
 });
-
-export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
