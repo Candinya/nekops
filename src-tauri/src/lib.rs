ddf5f9f34ff4e41072490fba58b5bbcf781974c3
Nya Candy <dev@candinya.com>
2024-04-21 01:33:30 +0800

    feat: allow only single instance
    not sure if this can work correctly, but at least let's have a try

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,24 @@
+// use tauri::{Manager};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  args: Vec<String>,
+  cwd: String,
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+            // app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap(); // Not working for tauri v2
+        }))
         // .plugin(tauri_plugin_window::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
