6d184d4612b45c61c539acc8e953f9f0e0e21f4d
Nya Candy <dev@candinya.com>
2024-04-24 00:14:47 +0800

    feat: also search ips

--- src/search/servers.ts
+++ src/search/servers.ts
@@ -1,4 +1,9 @@
-import type { Server } from "@/types/server.ts";
+import type { IP, Server } from "@/types/server.ts";
+
+const matchIP = (key: string, ip: IP) =>
+  ip.address.includes(key) ||
+  ip.alias.includes(key) ||
+  ip.comment.includes(key);
 
 export const searchServers = (searchInput: string, allServers: Server[]) =>
   searchInput === ""
@@ -6,11 +11,19 @@ export const searchServers = (searchInput: string, allServers: Server[]) =>
     : allServers.filter((server) => {
         for (const key of searchInput.toLowerCase().split(/\s+/)) {
           if (
-            key.length > 0 &&
+            key.length > 0 && // Is a valid search key
+            // 1. ID contain
             (server.id.toLowerCase().includes(key) ||
+              // 2. name contain
               server.name.toLowerCase().includes(key) ||
+              // 3. comment contain
               server.comment.toLowerCase().includes(key) ||
-              server.tags.findIndex((tag) => tag.toLowerCase() === key) != -1) // Tag full match
+              // 4. tags contain (full match)
+              server.tags.some((tag) => tag.toLowerCase() === key) ||
+              // 5.1. IP contain (public)
+              server.network.public.some((pubip) => matchIP(key, pubip)) ||
+              // 5.2. IP contain (private)
+              server.network.private.some((privip) => matchIP(key, privip)))
           ) {
             return true;
           }
--- src/search/snippets.ts
+++ src/search/snippets.ts
@@ -6,10 +6,13 @@ export const searchSnippets = (searchInput: string, allSnippets: Snippet[]) =>
     : allSnippets.filter((snippet) => {
         for (const key of searchInput.toLowerCase().split(/\s+/)) {
           if (
-            key.length > 0 &&
+            key.length > 0 && // Is a valid search key
+            // 1. name contain
             (snippet.name.toLowerCase().includes(key) ||
+              // 2. code contain
               snippet.code.toLowerCase().includes(key) ||
-              snippet.tags.findIndex((tag) => tag.toLowerCase() === key) != -1) // Tag full match
+              // 4. tags contain (full match)
+              snippet.tags.some((tag) => tag.toLowerCase() === key)) // Tag full match
           ) {
             return true;
           }
